d7a4220c694c4f3daa7d6169a773662a54efb2cf
Sanket <sanket.ksolanke@gmail.com>
2024-09-02 00:30:00 +0530

    nav

--- PassportApplication/PassportApplication/frontend/passport_app/src/app/modules/user-module/user-navbar/user-navbar.component.ts
+++ PassportApplication/PassportApplication/frontend/passport_app/src/app/modules/user-module/user-navbar/user-navbar.component.ts
@@ -15,7 +15,10 @@ import { AuthService } from '../../../services/auth.service';
   styleUrl: './user-navbar.component.css'
 })
 export class UserNavbarComponent implements  DoCheck {
- 
+  username: string = '';
+  profileName: string = '';
+  profileEmail: string = '';
+  profilePhone: string = '';
   isActivate: boolean = false;
   private router = inject(Router);
   private authService=inject(AuthService);
@@ -24,17 +27,34 @@ export class UserNavbarComponent implements  DoCheck {
 
   ngDoCheck(): void {
     this.checkUserStatus();
+    this.loadUserData();
   }
   checkUserStatus() {
     const loggedInUser = this.authService.getLoggedInUser();
     if(loggedInUser){
 
       this.isActivate = true;
+      
     }else{
       this.isActivate=false;
     }
     }
-
+    loadUserData(): void {
+      const loggedInUser = localStorage.getItem('currentUser');
+      if (loggedInUser) {
+        try {
+          const user = JSON.parse(loggedInUser);
+          this.username = `${user.firstname} ${user.lastname}`;
+          this.profileName = `${user.firstname} ${user.lastname}`;
+          this.profileEmail = user.email;
+          this.profilePhone = user.mobileNo;
+        } catch (error) {
+          console.error('Error parsing user data from local storage', error);
+        }
+      } else {
+        console.warn('No user data found in local storage');
+      }
+    }
  
   logout(): void {
     this.authService.logout();
